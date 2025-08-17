a20769751c8b4b396ecd0d5c947aea5f12dcddee
Torycd <vicidundun@gmail.com>
2025-08-18 00:42:54 +0100

    first commit

---
+++ basic/src/hooks/useApi.js
@@ -0,0 +1,43 @@
+import { useEffect, useReducer } from "react";
+
+function useApi(URL) {
+  const initialValue = {
+    // loading, success, failed
+    status: "",
+    data: [],
+    error: null,
+  };
+  function reducer(state, action) {
+    switch (action.type) {
+      case "start":
+        return { ...state, status: "loading" };
+      case "success":
+        return { status: "success", data: action.payload };
+      case "error":
+        return { ...state, status: "failed", error: action.payload };
+      default:
+        return { ...state };
+    }
+  }
+
+  const [{ status, data }, dispatch] = useReducer(reducer, initialValue);
+
+  useEffect(() => {
+    dispatch({ type: "start" });
+    const handleFetch = async () => {
+      try {
+        const response = await fetch(URL);
+        const data = await response.json();
+        dispatch({ type: "success", payload: data });
+      } catch (err) {
+        dispatch({ type: "failed", payload: err.message });
+        throw new Error("Failed to fecth data");
+      }
+    };
+    handleFetch();
+  }, [URL]);
+
+  return { status, data };
+}
+
+export default useApi;
