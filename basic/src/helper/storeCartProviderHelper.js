780ecb3872f535cf5073b2b1f2ac7f119a50cf12
Torycd <vicidundun@gmail.com>
2025-12-31 01:36:20 +0100

    lfg

--- basic/src/helper/storeCartProviderHelper.js
+++ basic/src/helper/storeCartProviderHelper.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { Children, createContext, useState } from "react";
 
 const StoreContext = createContext({
   data: [],
@@ -7,7 +7,7 @@ const StoreContext = createContext({
   deleteData: () => {},
 });
 
-function storeCartProviderHelper() {
+function StoreCartProviderHelper(children) {
   const [selectedData, setSelectedDate] = useState([]);
   const valueItem = {
     data: selectedData,
@@ -17,12 +17,36 @@ function storeCartProviderHelper() {
     setSelectedDate,
   };
   const handleAdd = (itm) => {
-    
+    setSelectedDate((previousSelected) => {
+      const exisiting = previousSelected.find((i) => i.id == itm.id);
+      if (exisiting) {
+        previousSelected.map((itmsPreviouslyFd) => {
+          itmsPreviouslyFd.id == itm.id
+            ? { ...itmsPreviouslyFd, quantity: itmsPreviouslyFd.quantity + 1 }
+            : itmsPreviouslyFd;
+        });
+      }
+      return [...previousSelected, { ...itm, quantity: 1 }];
+    });
   };
-  const handleReduce = () => {};
-  const handleDelete = () => {};
+  const handleReduce = (id) => {
+    setSelectedDate((previousSelected) => {
+      previousSelected.map((itmsPreviouslyFd) => {
+        itmsPreviouslyFd.id == id
+          ? { ...itmsPreviouslyFd, quantity: itmsPreviouslyFd.quantity - 1 }
+          : itmsPreviouslyFd;
+      });
+    });
+  };
+  const handleDelete = (id) => {
+    selectedData((previousSelected) => {
+      previousSelected.filter((itm) => itm.id !== id);
+    });
+  };
 
-  return <StoreContext.Provider value={valueItem}></StoreContext.Provider>;
+  return (
+    <StoreContext.Provider value={valueItem}>{children}</StoreContext.Provider>
+  );
 }
 
-export default storeCartProviderHelper;
+export default StoreCartProviderHelper;
